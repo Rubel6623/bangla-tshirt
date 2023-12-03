996c1cbfb4e832b4ac169676074231e1c4c04540
Rubel Rudra <rubelrudra27@gmail.com>
2023-12-03 23:27:19 +0600

    new project initialization

---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>this is a home tab</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam quasi beatae quis a nulla. Molestiae magnam vero sequi! Suscipit et quae laboriosam! Voluptatibus quos aut quod sapiente, iusto sunt!</p>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Component/Orderreview/OrderReview.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const OrderReview = () => {
+    return (
+        <div>
+            <h3>this is order review page...</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta tempore labore eveniet doloremque dolorum omnis, maiores unde nam facilis, minima sit. Aut corporis ea minus. Aperiam odit deleniti ipsa dolore.</p>
+        </div>
+    );
+};
+
+export default OrderReview;
