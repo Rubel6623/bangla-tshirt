ca38347b71de7d3ce597d64d0286bf7bf98b1773
Rubel Rudra <rubelrudra27@gmail.com>
2023-12-04 10:46:38 +0600

    add a new section

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -5,6 +5,9 @@ const Home = () => {
         <div>
             <h2>this is a home tab</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam quasi beatae quis a nulla. Molestiae magnam vero sequi! Suscipit et quae laboriosam! Voluptatibus quos aut quod sapiente, iusto sunt!</p>
+            <div className="git">
+                <a href='https://github.com/Rubel6623' target='_blank'>Github</a>
+            </div>
         </div>
     );
 };
